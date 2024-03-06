b46c67256b758a0c1bc328d19c45a0664e005da6
Sonja Dahlke <sonja-dahlke@web.de>
2024-03-05 16:17:31 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useRef } from "react";
+
+function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect =
+    (() => {
+      const canvas = canvasRef.current;
+      canvas.width = window.innerWidth * 2;
+      canvas.hight = window.innerHeight * 2;
+      canvas.style.width = `${window.innerWidth}px`;
+      canvas.style.hight = `${window.innerHeight}px`;
+
+      const context = canvas.getContext("2d");
+      context.scale(2, 2);
+      context.lineCap = "round";
+      context.strokeStyle = "black";
+      context.lineWidth = 5;
+      contextRef.current = context;
+    },
+    []);
+
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEven;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  return (
+    <>
+      retrun (
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+      />
+      );
+    </>
+  );
+}
+
+export default App;
