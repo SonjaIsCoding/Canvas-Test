abc323e3629646a92e3c42946a21e60c046dcc84
Ralf Siewert <ralf@actyvy.st>
2024-03-08 11:38:30 +0100

    add boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,89 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { useRef } from "react";
 
 function App() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
-
-  useEffect =
-    (() => {
-      const canvas = canvasRef.current;
-      canvas.width = window.innerWidth * 2;
-      canvas.hight = window.innerHeight * 2;
-      canvas.style.width = `${window.innerWidth}px`;
-      canvas.style.hight = `${window.innerHeight}px`;
-
-      const context = canvas.getContext("2d");
-      context.scale(2, 2);
-      context.lineCap = "round";
-      context.strokeStyle = "black";
-      context.lineWidth = 5;
-      contextRef.current = context;
+  const [drawingStartPosition, setDrawingStartPosition] = useState(null);
+  const [boxes, setBoxes] = useState([
+    {
+      x: 200,
+      y: 300,
+      height: 200,
+      width: 100,
     },
-    []);
+    {
+      x: 400,
+      y: 20,
+      height: 50,
+      width: 50,
+    },
+  ]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.backgroundColor = "yellow";
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    contextRef.current = context;
+  }, []);
+
+  useEffect(() => {
+    boxes.forEach((box) => {
+      contextRef.current.beginPath();
+      contextRef.current.moveTo(box.x, box.y);
+      contextRef.current.lineTo(box.x + box.width, box.y);
+      contextRef.current.lineTo(box.x + box.width, box.y + box.height);
+      contextRef.current.lineTo(box.x, box.y + box.height);
+      contextRef.current.lineTo(box.x, box.y);
+      contextRef.current.stroke();
+    });
+  }, [boxes]);
 
   const startDrawing = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.beginPath();
-    contextRef.current.moveTo(offsetX, offsetY);
+    setDrawingStartPosition({ x: offsetX, y: offsetY });
+    // contextRef.current.beginPath();
+    // contextRef.current.moveTo(offsetX, offsetY);
     setIsDrawing(true);
   };
 
-  const finishDrawing = () => {
-    contextRef.current.closePath();
+  const finishDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    if (drawingStartPosition) {
+      setBoxes([
+        ...boxes,
+        {
+          x: drawingStartPosition.x,
+          y: drawingStartPosition.y,
+          width: offsetX - drawingStartPosition.x,
+          height: offsetY - drawingStartPosition.y,
+        },
+      ]);
+      setDrawingStartPosition(null);
+    }
     setIsDrawing(false);
   };
 
-  const draw = ({ nativeEvent }) => {
-    if (!isDrawing) {
-      return;
-    }
-    const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY);
-    contextRef.current.stroke();
-  };
+  const draw = ({ nativeEvent }) => {};
 
   return (
     <>
-      retrun (
       <canvas
         onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
         onMouseMove={draw}
         ref={canvasRef}
       />
-      );
     </>
   );
 }
